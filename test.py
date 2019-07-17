418a27ef9e544bf24846715fee6a09ec0269e340
aokyut <epaperamic@gmail.com>
2019-07-17 23:32:14 +0900

    change test.py and test.ipynb

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ height,width=img.size
 center_ver=int(height/2)
 center_hor=int(width/2)
 img_p=np.array(img)[center_ver-64:center_ver+64,center_hor-64:center_hor+64,:]
-img2_p=np.array([img_p[round(i/2)-1,:,:] for i in range(1,257)])
-img3_p=np.array([img2_p[:,round(i/2)-1,:] for i in range(1,257)],dtype="uint8")
+img2_p=np.array([img_p[int(i/2),:,:] for i in range(256)])
+img3_p=np.array([img2_p[:,int(i/2),:] for i in range(256)],dtype="uint8")
 img3_p=img3_p.transpose(1,0,2)
 img3=Image.fromarray(img3_p)
 img3.show()
